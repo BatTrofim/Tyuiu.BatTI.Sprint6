35068eb746d7f5aceb3b43b62960769ac202592f
Trofim Bat <Tyuiu.BatTI@outlook.com>
2025-12-05 10:46:44 +0500

    Add project files.

---
+++ Tyuiu.BatTI.Sprint6.Task0.V9.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.BatTI.Sprint6.Task0.V9.Lib;
+
+namespace Tyuiu.BatTI.Sprint6.Task0.V9.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            Assert.AreEqual(-2.556, ds.Calculate(3));
+        }
+    }
+}
---
+++ Tyuiu.BatTI.Sprint6.Task0.V9/FormMain.cs
@@ -0,0 +1,23 @@
+using System.Drawing.Text;
+using Tyuiu.BatTI.Sprint6.Task0.V9.Lib;
+
+namespace Tyuiu.BatTI.Sprint6.Task0.V9
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
