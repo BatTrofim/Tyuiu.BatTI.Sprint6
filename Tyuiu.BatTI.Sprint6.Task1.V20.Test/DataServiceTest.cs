b3adf1d7a7ed79d5ca7f9dba91b79174ff2a5e59
Trofim Bat <Tyuiu.BatTI@outlook.com>
2025-12-05 19:19:47 +0500

    Task1

---
+++ Tyuiu.BatTI.Sprint6.Task1.V20.Test/DataServiceTest.cs
@@ -0,0 +1,13 @@
+﻿using Tyuiu.BatTI.Sprint6.Task1.V20.Lib;
+
+namespace Tyuiu.BatTI.Sprint6.Task1.V20.Test;
+
+[TestClass]
+public sealed class DataServiceTest
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        
+    }
+}
