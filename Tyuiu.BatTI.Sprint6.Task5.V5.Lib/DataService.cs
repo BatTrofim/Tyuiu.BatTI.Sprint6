bd5c1a77dc45be90c5bcc40982ccd1f93615c48b
Trofim Bat <Tyuiu.BatTI@outlook.com>
2025-12-08 14:40:36 +0500

    Task5

---
+++ Tyuiu.BatTI.Sprint6.Task5.V5.Lib/DataService.cs
@@ -0,0 +1,33 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+
+namespace Tyuiu.BatTI.Sprint6.Task5.V5.Lib
+{
+    public class DataService : ISprint6Task5V5
+    {
+        public int len = 0;
+        public double[] LoadFromDataFile(string path)
+        {
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    len++;
+                }
+            }
+            double[] num = new double[len];
+            int r = 0;
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    num[r] = Convert.ToDouble(line);
+                    r++;
+                }
+            }
+            num = num.Where(val => val != 0).ToArray();
+            return num;
+        }
+    }
+}
---
+++ Tyuiu.BatTI.Sprint6.Task5.V5/FormMain.cs
@@ -0,0 +1,10 @@
+namespace Tyuiu.BatTI.Sprint6.Task5.V5
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
