475dd10a1c634e1d9157c78e87bb50e42697ae36
Trofim Bat <Tyuiu.BatTI@outlook.com>
2025-12-08 13:45:57 +0500

    Task4

---
+++ Tyuiu.BatTI.Sprint6.Task4.V6.Lib/DataService.cs
@@ -0,0 +1,23 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+
+namespace Tyuiu.BatTI.Sprint6.Task4.V6.Lib
+{
+    public class DataService : ISprint6Task4V6
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            double[] res = new double[stopValue - startValue + 1];
+            int k = 0;
+            for (int x = startValue; x < stopValue + 1; x++)
+            {
+                if ((3 * x - 1) != 0)
+                {
+                    res[k] = Math.Round((3 * Math.Cos(x)) / (4 * x - 0.5) + Math.Sin(x) - 5 * x - 2, 2);
+                    k++;
+                }
+                else { res[k] = 0; k++; }
+            }
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.BatTI.Sprint6.Task4.V6/FormMain.cs
@@ -0,0 +1,10 @@
+namespace Tyuiu.BatTI.Sprint6.Task4.V6
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
