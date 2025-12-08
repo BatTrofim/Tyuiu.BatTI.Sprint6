a8a9fa938061caa43ebc36125e97d98f9e00a50f
Trofim Bat <Tyuiu.BatTI@outlook.com>
2025-12-08 15:59:48 +0500

    Task7

---
+++ Tyuiu.BatTI.Sprint6.Task7.V30.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.BatTI.Sprint6.Task7.V30.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.BatTI.Sprint6.Task7.V30/FormMain.cs
@@ -0,0 +1,25 @@
+namespace Tyuiu.BatTI.Sprint6.Task7.V30
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
