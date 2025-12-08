1049732301194bb52fc1fe34329a0f925056a053
Trofim Bat <Tyuiu.BatTI@outlook.com>
2025-12-08 13:28:11 +0500

    Task3

---
+++ Tyuiu.BatTI.Sprint6.Task3.V6.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using System.Numerics;
+using Tyuiu.BatTI.Sprint6.Task3.V6.Lib;
+
+namespace Tyuiu.BatTI.Sprint6.Task3.V6.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+        }
+    }
+}
---
+++ Tyuiu.BatTI.Sprint6.Task3.V6/FormMain.cs
@@ -0,0 +1,10 @@
+namespace Tyuiu.BatTI.Sprint6.Task3.V6
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
