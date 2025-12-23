214848ee4907443b64da314e1dfe313253f25b04
Trofim Bat <Tyuiu.BatTI@outlook.com>
2025-12-23 22:51:55 +0500

    Task2

--- Tyuiu.BatTI.Sprint6.Task2.V3/FormMain.Designer.cs
+++ Tyuiu.BatTI.Sprint6.Task2.V3/FormMain.Designer.cs
@@ -2,15 +2,21 @@
 {
     partial class FormMain
     {
-        /// <summary>
-        ///  Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Label labelTask;
+        private System.Windows.Forms.Label labelStart;
+        private System.Windows.Forms.Label labelStop;
+        private System.Windows.Forms.TextBox textBoxStartStep;
+        private System.Windows.Forms.TextBox textBoxStopStep;
+        private System.Windows.Forms.Button buttonDone;
+        private System.Windows.Forms.Button buttonHelp;
+        private System.Windows.Forms.DataGridView dataGridViewFunction;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnX;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnFx;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartFunction;
+        private System.Windows.Forms.Panel panelInput;
+        private System.Windows.Forms.Panel panelOutput;
 
-        /// <summary>
-        ///  Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,107 +26,193 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
-            textBoxStart = new TextBox();
-            textBoxEnd = new TextBox();
-            dataGridViewRes = new DataGridView();
-            Column1 = new DataGridViewTextBoxColumn();
-            Column2 = new DataGridViewTextBoxColumn();
-            dataServiceBindingSource = new BindingSource(components);
-            buttonResult = new Button();
-            ((System.ComponentModel.ISupportInitialize)dataGridViewRes).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)dataServiceBindingSource).BeginInit();
-            SuspendLayout();
-            // 
-            // textBoxStart
-            // 
-            textBoxStart.Location = new Point(30, 356);
-            textBoxStart.Multiline = true;
-            textBoxStart.Name = "textBoxStart";
-            textBoxStart.Size = new Size(86, 44);
-            textBoxStart.TabIndex = 0;
-            textBoxStart.Text = "textBoxStart";
-            textBoxStart.TextAlign = HorizontalAlignment.Center;
-            // 
-            // textBoxEnd
-            // 
-            textBoxEnd.Location = new Point(122, 356);
-            textBoxEnd.Multiline = true;
-            textBoxEnd.Name = "textBoxEnd";
-            textBoxEnd.Size = new Size(86, 44);
-            textBoxEnd.TabIndex = 1;
-            textBoxEnd.Text = "textBoxEnd";
-            // 
-            // dataGridViewRes
-            // 
-            dataGridViewRes.AutoGenerateColumns = false;
-            dataGridViewRes.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridViewRes.Columns.AddRange(new DataGridViewColumn[] { Column1, Column2 });
-            dataGridViewRes.DataSource = dataServiceBindingSource;
-            dataGridViewRes.Location = new Point(294, 12);
-            dataGridViewRes.Name = "dataGridViewRes";
-            dataGridViewRes.Size = new Size(240, 294);
-            dataGridViewRes.TabIndex = 2;
-            dataGridViewRes.CellContentClick += dataGridViewRes_CellContentClick;
-            // 
-            // Column1
-            // 
-            Column1.HeaderText = "Column1";
-            Column1.Name = "Column1";
-            // 
-            // Column2
-            // 
-            Column2.HeaderText = "Column2";
-            Column2.Name = "Column2";
-            // 
-            // dataServiceBindingSource
-            // 
-            dataServiceBindingSource.DataSource = typeof(Lib.DataService);
-            // 
-            // buttonResult
-            // 
-            buttonResult.BackColor = SystemColors.GrayText;
-            buttonResult.Location = new Point(262, 356);
-            buttonResult.Name = "buttonResult";
-            buttonResult.Size = new Size(86, 44);
-            buttonResult.TabIndex = 3;
-            buttonResult.Text = "Выполнить";
-            buttonResult.UseVisualStyleBackColor = false;
-            // 
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.labelTask = new System.Windows.Forms.Label();
+            this.panelInput = new System.Windows.Forms.Panel();
+            this.labelStart = new System.Windows.Forms.Label();
+            this.labelStop = new System.Windows.Forms.Label();
+            this.textBoxStartStep = new System.Windows.Forms.TextBox();
+            this.textBoxStopStep = new System.Windows.Forms.TextBox();
+            this.buttonDone = new System.Windows.Forms.Button();
+            this.buttonHelp = new System.Windows.Forms.Button();
+            this.panelOutput = new System.Windows.Forms.Panel();
+            this.dataGridViewFunction = new System.Windows.Forms.DataGridView();
+            this.ColumnX = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnFx = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.chartFunction = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.panelInput.SuspendLayout();
+            this.panelOutput.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewFunction)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartFunction)).BeginInit();
+            this.SuspendLayout();
+
+            // labelTask
+            this.labelTask.AutoSize = true;
+            this.labelTask.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelTask.Location = new System.Drawing.Point(12, 9);
+            this.labelTask.Name = "labelTask";
+            this.labelTask.Size = new System.Drawing.Size(350, 25);
+            this.labelTask.TabIndex = 0;
+            this.labelTask.Text = "Спринт 6 | Таск 2 | Вариант 3";
+
+            // panelInput
+            this.panelInput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelInput.Controls.Add(this.textBoxStopStep);
+            this.panelInput.Controls.Add(this.textBoxStartStep);
+            this.panelInput.Controls.Add(this.labelStop);
+            this.panelInput.Controls.Add(this.labelStart);
+            this.panelInput.Location = new System.Drawing.Point(12, 40);
+            this.panelInput.Name = "panelInput";
+            this.panelInput.Size = new System.Drawing.Size(250, 80);
+            this.panelInput.TabIndex = 1;
+
+            // labelStart
+            this.labelStart.AutoSize = true;
+            this.labelStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelStart.Location = new System.Drawing.Point(10, 15);
+            this.labelStart.Name = "labelStart";
+            this.labelStart.Size = new System.Drawing.Size(100, 20);
+            this.labelStart.TabIndex = 0;
+            this.labelStart.Text = "Старт шага:";
+
+            // labelStop
+            this.labelStop.AutoSize = true;
+            this.labelStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelStop.Location = new System.Drawing.Point(10, 45);
+            this.labelStop.Name = "labelStop";
+            this.labelStop.Size = new System.Drawing.Size(103, 20);
+            this.labelStop.TabIndex = 1;
+            this.labelStop.Text = "Конец шага:";
+
+            // textBoxStartStep
+            this.textBoxStartStep.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxStartStep.Location = new System.Drawing.Point(120, 12);
+            this.textBoxStartStep.Name = "textBoxStartStep";
+            this.textBoxStartStep.Size = new System.Drawing.Size(100, 26);
+            this.textBoxStartStep.TabIndex = 2;
+            this.textBoxStartStep.Text = "-5";
+
+            // textBoxStopStep
+            this.textBoxStopStep.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxStopStep.Location = new System.Drawing.Point(120, 42);
+            this.textBoxStopStep.Name = "textBoxStopStep";
+            this.textBoxStopStep.Size = new System.Drawing.Size(100, 26);
+            this.textBoxStopStep.TabIndex = 3;
+            this.textBoxStopStep.Text = "5";
+
+            // buttonDone - ВЫПОЛНИТЬ (зеленый)
+            this.buttonDone.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.buttonDone.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonDone.ForeColor = System.Drawing.Color.White;
+            this.buttonDone.Location = new System.Drawing.Point(280, 40);
+            this.buttonDone.Name = "buttonDone";
+            this.buttonDone.Size = new System.Drawing.Size(150, 60);
+            this.buttonDone.TabIndex = 2;
+            this.buttonDone.Text = "ВЫПОЛНИТЬ";
+            this.buttonDone.UseVisualStyleBackColor = false;
+            this.buttonDone.Click += new System.EventHandler(this.buttonDone_Click);
+            this.buttonDone.MouseDown += new System.Windows.Forms.MouseEventHandler(this.buttonDone_MouseDown);
+            this.buttonDone.MouseEnter += new System.EventHandler(this.buttonDone_MouseEnter);
+            this.buttonDone.MouseLeave += new System.EventHandler(this.buttonDone_MouseLeave);
+
+            // buttonHelp - СПРАВКА (синий)
+            this.buttonHelp.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.buttonHelp.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonHelp.ForeColor = System.Drawing.Color.White;
+            this.buttonHelp.Location = new System.Drawing.Point(450, 40);
+            this.buttonHelp.Name = "buttonHelp";
+            this.buttonHelp.Size = new System.Drawing.Size(100, 60);
+            this.buttonHelp.TabIndex = 3;
+            this.buttonHelp.Text = "СПРАВКА";
+            this.buttonHelp.UseVisualStyleBackColor = false;
+            this.buttonHelp.Click += new System.EventHandler(this.buttonHelp_Click);
+
+            // panelOutput
+            this.panelOutput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelOutput.Controls.Add(this.dataGridViewFunction);
+            this.panelOutput.Location = new System.Drawing.Point(12, 130);
+            this.panelOutput.Name = "panelOutput";
+            this.panelOutput.Size = new System.Drawing.Size(300, 350);
+            this.panelOutput.TabIndex = 4;
+
+            // dataGridViewFunction
+            this.dataGridViewFunction.AllowUserToAddRows = false;
+            this.dataGridViewFunction.AllowUserToDeleteRows = false;
+            this.dataGridViewFunction.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewFunction.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColumnX,
+            this.ColumnFx});
+            this.dataGridViewFunction.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridViewFunction.Location = new System.Drawing.Point(0, 0);
+            this.dataGridViewFunction.Name = "dataGridViewFunction";
+            this.dataGridViewFunction.ReadOnly = true;
+            this.dataGridViewFunction.RowHeadersVisible = false;
+            this.dataGridViewFunction.RowHeadersWidth = 51;
+            this.dataGridViewFunction.RowTemplate.Height = 24;
+            this.dataGridViewFunction.Size = new System.Drawing.Size(298, 348);
+            this.dataGridViewFunction.TabIndex = 0;
+
+            // ColumnX
+            this.ColumnX.HeaderText = "X";
+            this.ColumnX.MinimumWidth = 6;
+            this.ColumnX.Name = "ColumnX";
+            this.ColumnX.ReadOnly = true;
+            this.ColumnX.Width = 125;
+
+            // ColumnFx
+            this.ColumnFx.HeaderText = "F(x)";
+            this.ColumnFx.MinimumWidth = 6;
+            this.ColumnFx.Name = "ColumnFx";
+            this.ColumnFx.ReadOnly = true;
+            this.ColumnFx.Width = 125;
+
+            // chartFunction
+            this.chartFunction.BorderlineColor = System.Drawing.Color.Black;
+            this.chartFunction.BorderlineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Solid;
+            chartArea1.Name = "ChartArea1";
+            this.chartFunction.ChartAreas.Add(chartArea1);
+            this.chartFunction.Dock = System.Windows.Forms.DockStyle.Right;
+            legend1.Name = "Legend1";
+            this.chartFunction.Legends.Add(legend1);
+            this.chartFunction.Location = new System.Drawing.Point(325, 130);
+            this.chartFunction.Name = "chartFunction";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chartFunction.Series.Add(series1);
+            this.chartFunction.Size = new System.Drawing.Size(550, 350);
+            this.chartFunction.TabIndex = 5;
+            this.chartFunction.Text = "chart1";
+            this.chartFunction.Click += new System.EventHandler(this.chartFunction_Click);
+
             // FormMain
-            // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
-            AutoScaleMode = AutoScaleMode.Font;
-            BackColor = SystemColors.Control;
-            ClientSize = new Size(800, 450);
-            Controls.Add(buttonResult);
-            Controls.Add(dataGridViewRes);
-            Controls.Add(textBoxEnd);
-            Controls.Add(textBoxStart);
-            Name = "FormMain";
-            Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)dataGridViewRes).EndInit();
-            ((System.ComponentModel.ISupportInitialize)dataServiceBindingSource).EndInit();
-            ResumeLayout(false);
-            PerformLayout();
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(884, 491);
+            this.Controls.Add(this.chartFunction);
+            this.Controls.Add(this.panelOutput);
+            this.Controls.Add(this.buttonHelp);
+            this.Controls.Add(this.buttonDone);
+            this.Controls.Add(this.panelInput);
+            this.Controls.Add(this.labelTask);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Спринт 6 | Таск 2 | Вариант 3 | Бат Т.И.";
+            this.panelInput.ResumeLayout(false);
+            this.panelInput.PerformLayout();
+            this.panelOutput.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewFunction)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartFunction)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
         }
-
-        #endregion
-
-        private TextBox textBoxStart;
-        private TextBox textBoxEnd;
-        private DataGridView dataGridViewRes;
-        private Button buttonResult;
-        private DataGridViewTextBoxColumn Column1;
-        private DataGridViewTextBoxColumn Column2;
-        private BindingSource dataServiceBindingSource;
     }
 }
--- Tyuiu.BatTI.Sprint6.Task2.V3/FormMain.cs
+++ Tyuiu.BatTI.Sprint6.Task2.V3/FormMain.cs
@@ -1,89 +1,86 @@
-using System.Windows.Forms.VisualStyles;
-using Tyuiu.BatTI.Sprint6.Task2.V3.Lib;
-
+﻿using Tyuiu.BatTI.Sprint6.Task2.V3.Lib;
 namespace Tyuiu.BatTI.Sprint6.Task2.V3
 {
     public partial class FormMain : Form
     {
+        DataService ds = new DataService();
         public FormMain()
         {
             InitializeComponent();
         }
-        DataService ds = new DataService();
-        private void textBoxTask_TextChanged(object sender, EventArgs e)
-        {
 
-        }
-
-        private void textBoxEnd_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void labelEnd_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void buttonResult_Click(object sender, EventArgs e)
+        private void buttonDone_Click(object sender, EventArgs e)
         {
             try
             {
-                int start = Convert.ToInt32(textBoxStart.Text);
-                int end = Convert.ToInt32(textBoxEnd.Text);
+                // Î÷èùàåì äàííûå
+                dataGridViewFunction.Rows.Clear();
+                chartFunction.Series[0].Points.Clear();
 
-                int len = ds.GetMassFunction(start, end).Length;
+                // Ïîëó÷àåì çíà÷åíèÿ
+                int startStep = Convert.ToInt32(textBoxStartStep.Text);
+                int stopStep = Convert.ToInt32(textBoxStopStep.Text);
 
-                double[] valueArray;
-                valueArray = new double[len];
-                valueArray = ds.GetMassFunction(start, end);
+                int len = stopStep - startStep + 1;
+                double[] valueArray = ds.GetMassFunction(startStep, stopStep);
 
-                this.dataGridViewRes.Rows.Clear();
-                for (int i = 0; i <= len - 1; i++)
+                // Íàñòðàèâàåì çàãîëîâêè
+                chartFunction.Titles.Clear();
+                chartFunction.Titles.Add("sin(x)/(x+1.2) + cos(x)*7x - 2");
+
+                chartFunction.ChartAreas[0].AxisX.Title = "X";
+                chartFunction.ChartAreas[0].AxisY.Title = "Y";
+
+               
+                for (int i = 0; i < len; i++)
                 {
-                    this.dataGridViewRes.Rows.Add(Convert.ToString(start), Convert.ToString(valueArray[i]));
-                    start++;
+                   
+                    dataGridViewFunction.Rows.Add(
+                        Convert.ToString(startStep + i),
+                        Convert.ToString(valueArray[i])
+                    );
+
+                   
+                    chartFunction.Series[0].Points.AddXY(startStep + i, valueArray[i]);
                 }
+
+                
+                buttonDone.Text = "Успешно!";
             }
             catch
             {
-                MessageBox.Show("??????? ???????? ??????", "??????", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show("Введено неправильное значение", "Ошибка",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
 
-        private void buttonInfo_Click(object sender, EventArgs e)
+        private void buttonDone_MouseEnter(object sender, EventArgs e)
         {
-            MessageBox.Show("???? 2 ???????? ??????? ?????? ????-25-1 ?????????? ???? ?????????", "?????????", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            buttonDone.BackColor = Color.FromArgb(0, 200, 0); 
         }
 
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        private void buttonDone_MouseLeave(object sender, EventArgs e)
         {
-
-        }
-        private void buttonResult_MouseMove(object sender, EventArgs e)
-        {
-            buttonResult.BackColor = Color.Red;
+            buttonDone.BackColor = Color.FromArgb(0, 192, 0); 
         }
 
-
-        private void buttonResult_MouseLeave(object sender, EventArgs e)
+        private void buttonDone_MouseDown(object sender, MouseEventArgs e)
         {
-            buttonResult.BackColor = Color.Green;
+            buttonDone.BackColor = Color.FromArgb(0, 150, 0); 
         }
 
-        private void buttonResult_MouseClick(object sender, MouseEventArgs e)
+        private void buttonHelp_Click(object sender, EventArgs e)
         {
-            buttonResult.BackColor = Color.YellowGreen;
+            MessageBox.Show("Построить график:\r\n" +
+                          "F(x) = sin(x)/(x+1.2) + cos(x)*7x - 2\r\n" +
+                          "\r\nВыполнил: Бат Трофим Иванович, ПИНб-25-1\r\n" +
+                          "Вариант 3",
+                          "Справка", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
 
-        private void buttonResult_MouseEnter(object sender, EventArgs e)
+        private void chartFunction_Click(object sender, EventArgs e)
         {
-            buttonResult.BackColor = Color.Red;
-        }
-
-        private void dataGridViewRes_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-
+            // Îáðàáîò÷èê êëèêà ïî ãðàôèêó (ìîæíî îñòàâèòü ïóñòûì)
         }
     }
 }
