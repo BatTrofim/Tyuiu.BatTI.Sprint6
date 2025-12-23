5e49b24508ba9d2ae4f4603af340869574e84909
Trofim Bat <Tyuiu.BatTI@outlook.com>
2025-12-23 22:32:22 +0500

    Task1

--- Tyuiu.BatTI.Sprint6.Task1.V20/FormMain.Designer.cs
+++ Tyuiu.BatTI.Sprint6.Task1.V20/FormMain.Designer.cs
@@ -1,16 +1,31 @@
-﻿namespace Tyuiu.BatTI.Sprint6.Task1.V20
+﻿using System.Windows.Forms;
+using System.Xml.Linq;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Tyuiu.BatTI.Sprint6.Task1.V20
 {
     partial class FormMain
     {
         /// <summary>
-        ///  Required designer variable.
+        /// Обязательная переменная конструктора.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
+        // Контроли, которыми будем пользоваться в коде
+        private System.Windows.Forms.Label lblCondition;
+        private System.Windows.Forms.Label lblInput;
+        private System.Windows.Forms.Label lblStart;
+        private System.Windows.Forms.Label lblEnd;
+        private System.Windows.Forms.TextBox txtStart;
+        private System.Windows.Forms.TextBox txtEnd;
+        private System.Windows.Forms.Button btnHelp;
+        private System.Windows.Forms.Button btnExecute;
+        private System.Windows.Forms.GroupBox groupBoxOutput;
+        private System.Windows.Forms.TextBox txtResult;
+
         /// <summary>
-        ///  Clean up any resources being used.
+        /// Освободить все используемые ресурсы.
         /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,87 +35,139 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
+        #region Код, автоматически созданный конструктором форм Windows
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Требуемый метод для поддержки конструктора — не изменяйте
+        /// содержимое этого метода с помощью редактора кода.
         /// </summary>
         private void InitializeComponent()
         {
-            textBoxVarStart = new TextBox();;
-            textBoxVarStop = new TextBox();
-            textBoxResult = new TextBox();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            lblCondition = new Label();
+            lblInput = new Label();
+            lblStart = new Label();
+            lblEnd = new Label();
+            txtStart = new TextBox();
+            txtEnd = new TextBox();
+            btnHelp = new Button();
+            btnExecute = new Button();
+            groupBoxOutput = new GroupBox();
+            txtResult = new TextBox();
+            groupBoxOutput.SuspendLayout();
             SuspendLayout();
             // 
-            // textBoxVarStart
+            // lblCondition
             // 
-            textBoxVarStart.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
-            textBoxVarStart.Location = new Point(12, 361);
-            textBoxVarStart.Name = "textBoxVarStart";
-            textBoxVarStart.Size = new Size(124, 33);
-            textBoxVarStart.TabIndex = 0;
+            lblCondition.BorderStyle = BorderStyle.FixedSingle;
+            lblCondition.Location = new Point(10, 10);
+            lblCondition.Name = "lblCondition";
+            lblCondition.Size = new Size(520, 220);
+            lblCondition.TabIndex = 0;
+            lblCondition.Text = resources.GetString("lblCondition.Text");
             // 
-            // label1
+            // lblInput
             // 
+            lblInput.Location = new Point(10, 240);
+            lblInput.Name = "lblInput";
+            lblInput.Size = new Size(200, 20);
+            lblInput.TabIndex = 1;
+            lblInput.Text = "Ввод данных:";
             // 
-            // label2
+            // lblStart
             // 
+            lblStart.Location = new Point(10, 270);
+            lblStart.Name = "lblStart";
+            lblStart.Size = new Size(120, 20);
+            lblStart.TabIndex = 2;
+            lblStart.Text = "Старт шага:";
             // 
-            // textBoxVarStop
+            // lblEnd
             // 
-            textBoxVarStop.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
-            textBoxVarStop.Location = new Point(142, 361);
-            textBoxVarStop.Name = "textBoxVarStop";
-            textBoxVarStop.Size = new Size(124, 33);
-            textBoxVarStop.TabIndex = 3;
+            lblEnd.Location = new Point(230, 270);
+            lblEnd.Name = "lblEnd";
+            lblEnd.Size = new Size(120, 20);
+            lblEnd.TabIndex = 4;
+            lblEnd.Text = "Конец шага:";
             // 
-            // textBoxResult
+            // txtStart
             // 
-            textBoxResult.Location = new Point(272, 167);
-            textBoxResult.Multiline = true;
-            textBoxResult.Name = "textBoxResult";
-            textBoxResult.Size = new Size(167, 227);
-            textBoxResult.TabIndex = 4;
+            txtStart.Location = new Point(130, 266);
+            txtStart.Name = "txtStart";
+            txtStart.Size = new Size(80, 27);
+            txtStart.TabIndex = 3;
+            txtStart.Text = "-5";
             // 
-            // label3
+            // txtEnd
             // 
-            label3.AutoSize = true;
-            label3.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
-            label3.Location = new Point(272, 139);
-            label3.Name = "label3";
-            label3.Size = new Size(62, 25);
-            label3.TabIndex = 5;
-            label3.Text = "Ответ";
+            txtEnd.Location = new Point(350, 266);
+            txtEnd.Name = "txtEnd";
+            txtEnd.Size = new Size(80, 27);
+            txtEnd.TabIndex = 5;
+            txtEnd.Text = "5";
             // 
-            // buttonDone
+            // btnHelp
             // 
-
+            btnHelp.Location = new Point(10, 310);
+            btnHelp.Name = "btnHelp";
+            btnHelp.Size = new Size(90, 40);
+            btnHelp.TabIndex = 6;
+            btnHelp.Text = "Справка";
+            btnHelp.UseVisualStyleBackColor = true;
+            btnHelp.Click += btnHelp_Click_1;
             // 
-            // FormMain
+            // btnExecute
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-
-            Controls.Add(label3);
-            Controls.Add(textBoxResult);
-            Controls.Add(textBoxVarStop);
-
-            Controls.Add(textBoxVarStart);
-            Name = "FormMain";
-            Text = "Form1";
+            btnExecute.Location = new Point(110, 310);
+            btnExecute.Name = "btnExecute";
+            btnExecute.Size = new Size(110, 40);
+            btnExecute.TabIndex = 7;
+            btnExecute.Text = "Выполнить";
+            btnExecute.UseVisualStyleBackColor = true;
+            // 
+            // groupBoxOutput
+            // 
+            groupBoxOutput.Controls.Add(txtResult);
+            groupBoxOutput.Location = new Point(540, 10);
+            groupBoxOutput.Name = "groupBoxOutput";
+            groupBoxOutput.Size = new Size(330, 400);
+            groupBoxOutput.TabIndex = 8;
+            groupBoxOutput.TabStop = false;
+            groupBoxOutput.Text = "Вывод данных";
+            // 
+            // txtResult
+            // 
+            txtResult.Font = new System.Drawing.Font("Consolas", 10F);
+            txtResult.Location = new Point(10, 20);
+            txtResult.Multiline = true;
+            txtResult.Name = "txtResult";
+            txtResult.ReadOnly = true;
+            txtResult.ScrollBars = ScrollBars.Vertical;
+            txtResult.Size = new Size(310, 370);
+            txtResult.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            ClientSize = new Size(900, 420);
+            Controls.Add(lblCondition);
+            Controls.Add(lblInput);
+            Controls.Add(lblStart);
+            Controls.Add(txtStart);
+            Controls.Add(lblEnd);
+            Controls.Add(txtEnd);
+            Controls.Add(btnHelp);
+            Controls.Add(btnExecute);
+            Controls.Add(groupBoxOutput);
+            Font = new System.Drawing.Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Спринт 6 | Таск 1 | Вариант 20 | Бат Т.И.";
+            groupBoxOutput.ResumeLayout(false);
+            groupBoxOutput.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
-
-        private TextBox textBoxVarStart;
-
-        private TextBox textBoxVarStop;
-        private TextBox textBoxResult;
-        private Label label3;
-
     }
 }
--- Tyuiu.BatTI.Sprint6.Task1.V20/FormMain.cs
+++ Tyuiu.BatTI.Sprint6.Task1.V20/FormMain.cs
@@ -1,3 +1,4 @@
+﻿using System.Globalization;
 using Tyuiu.BatTI.Sprint6.Task1.V20.Lib;
 
 namespace Tyuiu.BatTI.Sprint6.Task1.V20
@@ -7,6 +8,70 @@ namespace Tyuiu.BatTI.Sprint6.Task1.V20
         public FormMain()
         {
             InitializeComponent();
+            btnExecute.Click += BtnExecute_Click;
+            btnHelp.Click += BtnHelp_Click;
+        }
+
+        private void BtnHelp_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(
+                "Выполнено студентом группы ПИНб-25-1 Бат Т.И.\n",
+                "Справка",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+        }
+
+        private void BtnExecute_Click(object sender, EventArgs e)
+        {
+            txtResult.Clear();
+
+            // ×èòàåì äèàïàçîí
+            if (!double.TryParse(txtStart.Text.Replace(',', '.'), NumberStyles.Any, CultureInfo.InvariantCulture, out double start))
+            {
+                MessageBox.Show("Введено неправильное значение.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (!double.TryParse(txtEnd.Text.Replace(',', '.'), NumberStyles.Any, CultureInfo.InvariantCulture, out double end))
+            {
+                MessageBox.Show("Введено неправильное значение.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            double step = start <= end ? 1.0 : -1.0; 
+
+            txtResult.AppendText(string.Format("{0,-10} {1,12}\r\n", "x", "f(x)"));
+            txtResult.AppendText(new string('-', 26) + "\r\n");
+
+            const double eps = 1e-12;
+
+            for (double x = start; step > 0 ? x <= end + 1e-9 : x >= end - 1e-9; x += step)
+            {
+                double denom = Math.Cos(x) - 2.0 * x;
+                double fx;
+
+                if (Math.Abs(denom) < eps)
+                {
+                    fx = 0.0; 
+                }
+                else
+                {
+                    fx = (2.0 * x - 3.0) / denom + 5.0 * x - Math.Sin(x);
+                }
+
+                double fxRounded = Math.Round(fx, 2);
+
+                
+                string xs = (Math.Abs(x - Math.Round(x)) < 1e-9) ? ((int)Math.Round(x)).ToString() : x.ToString("F2", CultureInfo.CurrentCulture);
+                string fxs = fxRounded.ToString("F2", CultureInfo.CurrentCulture);
+
+                txtResult.AppendText(string.Format("{0,-10} {1,12}\r\n", xs, fxs));
+            }
+        }
+
+        private void btnHelp_Click_1(object sender, EventArgs e)
+        {
+
         }
     }
 }
