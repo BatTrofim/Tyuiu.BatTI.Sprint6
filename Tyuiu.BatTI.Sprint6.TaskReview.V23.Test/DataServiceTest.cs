9148696564036d160df7e6fa529ac64d10027423
Trofim Bat <Tyuiu.BatTI@outlook.com>
2025-12-24 10:51:00 +0500

    TaskReview

--- Tyuiu.BatTI.Sprint6.TaskReview.V23.Test/DataServiceTest.cs
+++ Tyuiu.BatTI.Sprint6.TaskReview.V23.Test/DataServiceTest.cs
@@ -6,33 +6,18 @@ namespace Tyuiu.BatTI.Sprint6.TaskReview.V23.Test
     public sealed class DataServiceTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ValidGetMatrix()
         {
-            Random rand = new Random();
             DataService ds = new DataService();
-            int n = Convert.ToInt32(Console.ReadLine());
-            int m = Convert.ToInt32(Console.ReadLine());
-            int n1 = Convert.ToInt32(Console.ReadLine());
-            int n2 = Convert.ToInt32(Console.ReadLine());
 
-            int[,] wait = new int[n, m];
-            int[,] res = ds.GetMatrix(n, m, n1, n2);
-            for (int i = 0; i < n; i++)
-            {
-                for (int j = 0; j < m; j++)
-                {
-                    if (j % 2 == 0)
-                    {
-                        wait[i, j] = rand.Next(n1, n2);
-                    }
-                    else
-                    {
-                        wait[i, j] = (int)Math.Pow(Convert.ToInt32(wait[i, j - 1]), 2);
-                    }
-                }
-            }
-            Assert.AreEqual(wait, res);
+            int[,] array = new int[5, 5] { {8, 64, 3, 9, 10 },
+                                            { 7, 49, 1, 1, 9 },
+                                            { 0, 0, 4, 16, 8 },
+                                            { 5, 25, 2, 4, 9 },
+                                            { 3, 9, 0, 0, 1 } };
 
+            int res = ds.resultGetMatrix(array, 3, 1, 4);
+            int wait = 64;
         }
     }
 }
